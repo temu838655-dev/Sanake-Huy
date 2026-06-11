332b1183aa6e3f48f6a4da77fa4505b57648be2e
temu838655-dev <temu838655@gmail.com>
2026-06-11 08:41:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,66 @@
-        from kivy.app import App
+from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
-from kivy.properties import ListProperty
+from kivy.graphics import Rectangle, Color
 from random import randint
-from kivy.core.window import Window
+
+GRID = 20
+SIZE = 20
 
 class SnakeGame(Widget):
-    snake = ListProperty([(10, 10), (10, 11), (10, 12)])
-    food = ListProperty([5, 5])
-    direction = ListProperty([0, 1])
-    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        self.snake = [(10, 10), (10, 11), (10, 12)]
+        self.direction = (0, 1)
+        self.food = (5, 5)
+
         Clock.schedule_interval(self.update, 0.2)
-        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
-        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+        self.bind(pos=self.redraw, size=self.redraw)
 
-    def _keyboard_closed(self):
-        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
-        self._keyboard = None
-
-    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        if keycode[1] == 'up' and self.direction != [0, -1]: self.direction = [0, 1]
-        elif keycode[1] == 'down' and self.direction != [0, 1]: self.direction = [0, -1]
-        elif keycode[1] == 'left' and self.direction != [1, 0]: self.direction = [-1, 0]
-        elif keycode[1] == 'right' and self.direction != [-1, 0]: self.direction = [1, 0]
-        return True
+    def on_touch_down(self, touch):
+        x, y = touch.pos
+        if x < self.width / 2:
+            self.direction = (-1, 0)
+        else:
+            self.direction = (1, 0)
 
     def update(self, dt):
-        new_head = (self.snake[-1][0] + self.direction[0], self.snake[-1][1] + self.direction[1])
-        if new_head == tuple(self.food):
-            self.food = [randint(0, 19), randint(0, 19)]
+        hx, hy = self.snake[-1]
+        dx, dy = self.direction
+
+        new_head = (hx + dx, hy + dy)
+
+        # ăn food
+        if new_head == self.food:
+            self.food = (randint(0, 19), randint(0, 19))
         else:
             self.snake.pop(0)
+
+        # game over đơn giản
+        if new_head in self.snake:
+            self.snake = [(10, 10), (10, 11), (10, 12)]
+
         self.snake.append(new_head)
+        self.redraw()
+
+    def redraw(self, *args):
+        self.canvas.clear()
+
+        with self.canvas:
+            # food
+            Color(1, 0, 0)
+            Rectangle(pos=(self.food[0]*SIZE, self.food[1]*SIZE), size=(SIZE, SIZE))
+
+            # snake
+            Color(0, 1, 0)
+            for x, y in self.snake:
+                Rectangle(pos=(x*SIZE, y*SIZE), size=(SIZE, SIZE))
+
 
 class SnakeApp(App):
     def build(self):
         return SnakeGame()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     SnakeApp().run()
-    
